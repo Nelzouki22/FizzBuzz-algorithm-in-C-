8627a9d9436e7cd89377ddf255f1924fe4c7712b
NADIR ELZOUKI <elzoukigroup2018@gmail.com>
2024-06-26 22:52:26 -0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+class Program
+{
+    static void FizzBuzz(int n)
+    {
+        for (int i = 1; i <= n; i++)
+        {
+            if (i % 3 == 0 && i % 5 == 0)
+            {
+                Console.WriteLine("FizzBuzz");
+            }
+            else if (i % 3 == 0)
+            {
+                Console.WriteLine("Fizz");
+            }
+            else if (i % 5 == 0)
+            {
+                Console.WriteLine("Buzz");
+            }
+            else
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+
+    static void Main(string[] args)
+    {
+        // Call the function to print FizzBuzz from 1 to 100
+        FizzBuzz(100);
+    }
+}
+
